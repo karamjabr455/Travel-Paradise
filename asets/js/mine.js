7e21038389fc85e81fb851c3ba64ff7263a272fa
karamjabr455 <129905901+karamjabr455@users.noreply.github.com>
2024-04-24 00:59:01 -0700

    Add files via upload

---
+++ asets/js/mine.js
@@ -0,0 +1,20 @@
+// JQuery library related to date entry
+$( function() {
+    $( "#inputcheckin" ).datepicker();
+    $( "#inputcheckOut" ).datepicker();
+//slider...................
+$('.level-slider').slick({
+   dots:true,
+   slidesToShow:1,
+   
+   arrows:true,
+   
+  });
+
+
+   
+
+  } );
+
+
+
